8ce50bdab48e90bb613da25da8f7bcf792234014
Blairg23 <blair.gemmer@gmail.com>
2016-09-21 03:13:49 -0600

    Argument Parsing and Endpoints

--- sandbox/api.py
+++ sandbox/api.py
@@ -1,8 +1,11 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
 api = Api(app)
+
+parser = reqparse.RequestParser()
+parser.add_argument('data', type=str, help='Todo task.')
 
 todos = {}
 
@@ -11,10 +14,11 @@ class TodoSimple(Resource):
         return {todo_id: todos[todo_id]}
 
     def put(self, todo_id):
-        todos[todo_id] = request.form['data']
+    	args = parser.parse_args(strict=True)
+        todos[todo_id] = args['data']
         return {todo_id: todos[todo_id]}
 
-api.add_resource(TodoSimple, '/<string:todo_id>')
+api.add_resource(TodoSimple, '/todo/<int:todo_id>', endpoint='todo_ep')
 
 if __name__ == '__main__':
     app.run(debug=True)
