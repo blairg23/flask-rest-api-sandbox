efc7e14c673de29029c2e36f438b775e53183bd6
Blairg23 <blair.gemmer@gmail.com>
2016-09-21 03:23:12 -0600

    Full Example

--- sandbox/api.py
+++ sandbox/api.py
@@ -1,44 +1,61 @@
-from flask import Flask, request
-from flask_restful import Resource, Api, reqparse, fields, marshal_with
+from flask import Flask
+from flask_restful import reqparse, abort, Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
 
-parser = reqparse.RequestParser()
-parser.add_argument('data', type=str, help='Todo task.')
-
-todos = {}
-
-resource_fields = {
-    'task':   fields.String,
-    'uri':    fields.Url('todo_ep')
+TODOS = {
+    'todo1': {'task': 'build an API'},
+    'todo2': {'task': '?????'},
+    'todo3': {'task': 'profit!'},
 }
 
-class TodoDao(object):
-    def __init__(self, todo_id, task):
-        self.todo_id = todo_id
-        self.task = task
 
-        # This field will not be sent in the response
-        self.status = 'active'
+def abort_if_todo_doesnt_exist(todo_id):
+    if todo_id not in TODOS:
+        abort(404, message="Todo {} doesn't exist".format(todo_id))
 
+parser = reqparse.RequestParser()
+parser.add_argument('task')
+
+
+# Todo
+# shows a single todo item and lets you delete a todo item
 class Todo(Resource):
-    @marshal_with(resource_fields)
-    def get(self, **kwargs):
-        return TodoDao(todo_id='1', task='Remember the milk')
-
-class TodoSimple(Resource):
     def get(self, todo_id):
-        return {todo_id: todos[todo_id]}
+        abort_if_todo_doesnt_exist(todo_id)
+        return TODOS[todo_id]
+
+    def delete(self, todo_id):
+        abort_if_todo_doesnt_exist(todo_id)
+        del TODOS[todo_id]
+        return '', 204
 
     def put(self, todo_id):
-    	args = parser.parse_args(strict=True)
-        todos[todo_id] = args['data']
-        return {todo_id: todos[todo_id]}
+        args = parser.parse_args()
+        task = {'task': args['task']}
+        TODOS[todo_id] = task
+        return task, 201
 
 
-api.add_resource(Todo, '/todo/<int:todo_id>', endpoint='todo')
-api.add_resource(TodoSimple, '/todo/<int:todo_id>', endpoint='todo_ep')
+# TodoList
+# shows a list of all todos, and lets you POST to add new tasks
+class TodoList(Resource):
+    def get(self):
+        return TODOS
+
+    def post(self):
+        args = parser.parse_args()
+        todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
+        todo_id = 'todo%i' % todo_id
+        TODOS[todo_id] = {'task': args['task']}
+        return TODOS[todo_id], 201
+
+##
+## Actually setup the Api resource routing here
+##
+api.add_resource(TodoList, '/todos')
+api.add_resource(Todo, '/todos/<todo_id>')
 
 
 if __name__ == '__main__':
