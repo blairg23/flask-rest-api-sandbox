03bf962736078952462763a757dccc835a8a30ff
blairg23 <blairg23@gmail.com>
2016-10-16 15:26:28 -0600

    Updated to run gevent mu lti-threaded server

--- sandbox/__main__.py
+++ sandbox/__main__.py
@@ -1,4 +1,7 @@
 from sandbox import app as application
+from gevent.wsgi import WSGIServer
 
 if __name__ == '__main__':
-	application.run(debug=False)
+	#application.run(debug=False)
+	http_server = WSGIServer(('', 5000), application)
+	http_server.serve_forever()
