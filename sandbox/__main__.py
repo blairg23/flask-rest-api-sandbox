ce9c241db0daa42776580fd968a58034635b10f3
Blair Gemmer <6225764+blairg23@users.noreply.github.com>
2021-10-06 15:06:52 -0600

    Update __main__.py

--- sandbox/__main__.py
+++ sandbox/__main__.py
@@ -2,6 +2,5 @@ from sandbox import app as application
 from gevent.pywsgi import WSGIServer
 
 if __name__ == '__main__':
-	#application.run(debug=False)
 	http_server = WSGIServer(('', 5000), application)
 	http_server.serve_forever()
